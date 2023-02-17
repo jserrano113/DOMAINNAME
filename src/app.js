2b4da3c3ede9e1a90a549da6478a0bdfc24690b0
jserrano113 <jserrano113@gmail.com>
2023-02-17 23:57:25 +0000

    comment

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "raccon"];
+  let domain = [".com", ".net", ".gov", ".us"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {
+          domainName.push("  " + pronoun[a] + adj[b] + noun[c] + domain[d]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
 };
